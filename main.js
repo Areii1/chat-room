bbee84b13346c42d21aa36c74ad499844bb20aa2
Areii1 <ari.jaaskelainen1@gmail.com>
2016-05-28 09:19:10 +0300

    Create text input field and submit button, that appends the given input to the HTML.

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('hello chat room');
+var form = document.getElementById('text-input');
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+	var comment = document.getElementsByName('comment')[0].value;
+	document.getElementById('text-output').innerHTML = comment;
+});
