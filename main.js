7a2506073a2ad4ed6ec4cd7b44f0deacb4873737
Areii1 <ari.jaaskelainen1@gmail.com>
2016-05-28 09:25:45 +0300

    Refactor many <p> elements to be created rather than modify only one <p> element.

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ var form = document.getElementById('text-input');
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
 	var comment = document.getElementsByName('comment')[0].value;
-	document.getElementById('text-output').innerHTML = comment;
+	var pElement = document.createElement('p');
+	pElement.innerHTML = comment;
+	document.getElementById('chat-area').appendChild(pElement);
 });
