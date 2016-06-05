b12bfaef6ac8d5a971bae567f1b177f1bc89078c
Areii1 <ari.jaaskelainen1@gmail.com>
2016-06-05 23:12:08 +0300

    Create register form link to database.

---
+++ backend/create-user.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'connect-to-db.php';
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = 'INSERT INTO user (username, password) VALUES (?, ?)';
+$query = $handler->prepare($sql);
+
+$query->execute([$username, $password]);
+?>
