754d1fbe7b38f0352701aab4468d9fb4e43303d9
Areii1 <ari.jaaskelainen1@gmail.com>
2016-06-05 21:24:01 +0300

    Change root password

--- backend/connect-to-db.php
+++ backend/connect-to-db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-	$handler = new PDO('mysql:host=localhost;dbname=chat-room', 'root', '');
+	$handler = new PDO('mysql:host=localhost;dbname=chat-room', 'root', 'admin');
 	$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
