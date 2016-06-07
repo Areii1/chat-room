354261cb2e6703d32bddddab8be242e45159a318
Areii1 <ari.jaaskelainen1@gmail.com>
2016-06-07 15:27:00 +0300

    Add password validation.

--- backend/validate-user.php
+++ backend/validate-user.php
@@ -7,8 +7,17 @@ if ($username && $password) {
 	require 'connect-to-db.php';
 
 	$query = $handler->query("SELECT * FROM user WHERE username='$username'");
-	$queryString = json_encode(($query->fetchAll(PDO::FETCH_ASSOC)));
-	echo $queryString;
+	$queryString = json_encode(($query->fetch(PDO::FETCH_ASSOC)));
+
+	$result = json_decode($queryString, true);
+	$dbpassword = $result['password'];
+
+	if ($dbpassword == $password) {
+		echo 'success';
+	}
+	else {
+		echo 'failure';
+	}
 }
 else {
 	die("Please enter a username and a password");
